1287a348f133967cb7853a71d5b87f375f95269f
Sébastien Vallet <sebastien.vallet89@gmail.com>
2020-01-31 10:53:49 +0100

    initial commit

---
+++ src/test/java/com/cesi/jee/JeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesi.jee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
